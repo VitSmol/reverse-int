91a43189a76f06550b324266834fd1f28f5aa1c5
VitSmol <smolitskiy@mail.ru>
2021-08-05 17:27:39 +0300

    feat: add JS code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let arr = String(Math.abs(n)).split("");
+    let newStr = "";
+    for (let i = arr.length - 1; i >= 0; i--) {
+        newStr += arr[i];
+    }
+    newStr = Number(newStr)
+    return newStr;
 }
